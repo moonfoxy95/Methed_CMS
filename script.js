20cbb063ea633c53b18b04e290b04e88929c5cd5
VladLaptop <moonfoxy@icloud.com>
2025-04-05 16:21:07 +0300

    done lesson

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const cmsTitle = document.querySelector('.modal__title');
+const cmsForm = document.querySelector('.modal__form');
+const cmsCheckboxDiscount = document.querySelector('.modal__checkbox');
+const cmsInputDiscount = document.querySelector('.modal__input_discount');
